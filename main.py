2f3777d7e861d5ad1bc0e49b2853e5df0e7cb889
Juan Yepez <juanyepez@ucr-secure-01-fs-10-12-133-141.wnet.ucr.edu>
2023-04-17 10:33:34 -0700

    made the simplest version of adding a song

---
+++ main.py
@@ -0,0 +1,7 @@
+# having data stored in user input
+
+songTitle = input("Enter song title: ")
+songBy = input("Enter artist of song: ")
+numStreams = input("Enter number of streams: ")
+
+print(songTitle + " by " + songBy + " has " + numStreams + " streams ")
