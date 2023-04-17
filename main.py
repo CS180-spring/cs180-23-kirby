7f21a4c273d568b20ca7d84221840ce0ab9819c1
Juan Yepez <juanyepez@ucr-secure-01-fs-10-12-133-141.wnet.ucr.edu>
2023-04-17 10:49:21 -0700

    made sure that stream input is a numerical value

--- main.py
+++ main.py
@@ -2,6 +2,14 @@
 
 songTitle = input("Enter song title: ")
 songBy = input("Enter artist of song: ")
-numStreams = input("Enter number of streams: ")
 
-print(songTitle + " by " + songBy + " has " + numStreams + " streams ")
+while True:
+    streamInput = input("Enter number of streams: ")
+    if streamInput.isdigit():
+        streamNum = streamInput
+        break
+    else:
+        print("That was not a valid number. Please try again.")
+
+
+print(songTitle + " by " + songBy + " has " + streamNum + " streams ")
