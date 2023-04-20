85483ab9bffd149aafa36a498be47edbe9d6dc1b
Juan Yepez <juanyepez@ucr-secure-06-fs-10-12-153-96.wnet.ucr.edu>
2023-04-20 15:28:56 -0700

    inlab fix

--- delete.cpp
+++ delete.cpp
@@ -3,14 +3,14 @@
 #include <string>
 #include <vector>
 #include <sstream>
-#include "main.cpp"
 
 // params being passed are the DB(kirby vector)
 // and the strong title that we want to delete
 void deleteSong(vector<vector<string>> &vect, string song)
 
 {
-    int check = 0 for (int i = 1; i < vect.size(); i++)
+    int check = 0;
+    for (int i = 1; i < vect.size(); i++)
     {
         // comment
         if (song == vect[i][0])
