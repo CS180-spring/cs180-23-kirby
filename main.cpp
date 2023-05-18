fb9b885df00479663db7b2ecfdd298871647284b
Joseph Hu <77127695+JosephHu113@users.noreply.github.com>
2023-05-18 09:10:21 +0000

    Fixed Terminal

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main() {
     kirbydb database;
     string outputfile = "output.csv";
-    string terminalinput = "n/a"; //input, as long as it is not exit
+    int terminalinput = 10000; //some large value
     string userinput;
     Song* currentSongPointer; //Used to access current song pointer
 
@@ -17,28 +17,40 @@ int main() {
     cout << "Initialized KirbyDB database" << endl;
     cout << "Default Export File is output.csv" << endl;
 
-
-    while(terminalinput != "exit"){ //arbitrary number to stay in loop
+    while(terminalinput > -10){ //arbitrary number to stay in loop
         terminalprint();
+        cout << "\nThere are currently " << database.returnSongNum() << " songs in the database." << endl;
         cin >> terminalinput;
-        if(terminalinput == "add"){
-            cin.ignore();
-            addonesong(database);
+        if(terminalinput == 1){
+            system("clear");
+            addPrint();
+            cin >> terminalinput;
+            if(terminalinput == 1){
+                system("clear"); //only works on linux systems
+                cin.ignore();
+                addonesong(database);
+            }
+            else if(terminalinput == 2){
+                system("clear");
+                cin.ignore();
+                importfromfile(outputfile, database);
+            }
+            else{
+                cout << "Invalid Option. Returning to Terminal." << endl;
+            }
         }
-        else if(terminalinput == "import"){
-
-            cin.ignore();
-            importfromfile(outputfile, database);
-        }
-        else if(terminalinput == "delete"){
+        else if(terminalinput == 2){
+            system("clear");
             cout << "Enter the name of the song to delete: " << endl;
             cin >> userinput;
             database.removeSong(userinput);
         }
-        else if(terminalinput == "search"){
+        else if(terminalinput == 3){
+            system("clear");
             searchPrint();
+            cin.ignore();
             cin >> terminalinput;
-            if(terminalinput == "1"){
+            if(terminalinput == 1){
                 cout << "Enter the name of the song to search: " << endl;
                 cin.ignore();
                 getline(cin, userinput);
@@ -50,7 +62,7 @@ int main() {
                     cout << "Song does not exist" << endl;
                 }
             }
-            else if(terminalinput == "2"){
+            else if(terminalinput == 2){
                 cout << "Enter the name of the artist to search: " << endl;
                 cin.ignore();
                 getline(cin, userinput);
@@ -61,7 +73,7 @@ int main() {
                 //     cout << "Artist does not exist" << endl;
                 // }
             }
-            else if(terminalinput == "3"){
+            else if(terminalinput == 3){
                 cout << "Enter the name of the album to search: " << endl;
                 cin.ignore();
                 getline(cin, userinput);
@@ -73,33 +85,29 @@ int main() {
                 // }
             }
         }
-        
-        else if(terminalinput == "songlist"){
-            database.listsonglist();
-        }
-        else if(terminalinput == "export"){
-            database.exportsonglist();
-            cout << "Songs Exported" << endl;
-        }
-        else if(terminalinput == "exit"){
-            cout << "Exiting Program" << endl;
-        }
-        else if(terminalinput == "sizedb"){
-            cout << "There are currently " << database.returnSongNum() << " songs in the database." << endl;
-        }
-        else if(terminalinput == "createplaylist"){
+        else if(terminalinput == 4){
             cin.ignore();
             database.addPlaylist();
         }
-        else if(terminalinput == "addexisting"){
+        else if(terminalinput == 5){
+            cin.ignore();
+            database.listPlaylist();
+        }
+        else if(terminalinput == 6){
             cout << "Enter the name of the playlist to edit: " << endl;
             cin.ignore();
             getline(cin, userinput);
             database.modifyPlaylistSongs(userinput);
         }
-        else if(terminalinput == "playlist"){
-            cin.ignore();
-            database.listPlaylist();
+        else if(terminalinput == 7){
+            database.listsonglist();
+        }
+        else if(terminalinput == 9){
+            database.exportsonglist();
+            cout << "Songs Exported" << endl;
+        }
+        else if(terminalinput == 0){
+            cout << "Exiting Program" << endl;
         }
         else{
             cout << "Invalid Input" << endl;
